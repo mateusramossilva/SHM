82eb1383a1892d7e75759668a86b148b0e2c6957
mateusramossilva <mateusramos@4btaxtech.com.br>
2026-02-19 09:39:51 -0300

    Preparação para Deploy: Frontend e Backend

---
+++ backend/dist/automation/omie-secrets.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OMIE_CONFIG = void 0;
+exports.OMIE_CONFIG = {
+    APP_KEY: '7206168793824',
+    APP_SECRET: '898d9360e3410999f6a25427bafb7d5d',
+    BASE_URL: 'https://app.omie.com.br/api/v1'
+};
+//# sourceMappingURL=omie-secrets.js.map
